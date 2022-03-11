b839781bd73b0443f9033941c8c0c9a3261f0a63
heeyahnuh123 <heeyahnuh@gmail.com>
2022-03-10 18:18:32 -0800

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  *main - print exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
  *
@@ -6,6 +7,6 @@
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 return (1);
 }
