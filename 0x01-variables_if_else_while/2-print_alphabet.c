40a96e613bd2a98cdc024f4a762ed0fe226964bc
heeyahnuh123 <heeyahnuh@gmail.com>
2022-03-12 16:53:52 -0800

    commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
 
 /**
- * main - countdown of alphabets
- * from a - z separated by newline
+ *main - prints the alphabet in lowercase,
+ * followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
+    char ch;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	
-	for (n = 'a'; n = 'z'; n++)
-		putchar(n);
-		putchar('\n');
-		getch();
-
-	return (0);
+    for (ch = 'a'; ch <= 'z'; ch++)
+    {
+        putchar(ch);
+    }
+    putchar('\n');
+    return (0);
 }
