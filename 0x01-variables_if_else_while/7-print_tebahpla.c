6c6b499b769e53ed4b1f415c190e3ba9aacd6474
heeyahnuh123 <heeyahnuh@gmail.com>
2022-03-12 17:18:17 -0800

    commit

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -13,5 +13,5 @@ int main(void)
 	{putchar(alphabet);
 	}
 putchar('\n');
-return(0);
+return (0);
 }
