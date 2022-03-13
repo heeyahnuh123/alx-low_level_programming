45850003c1d28f5f5a81fe2977f340b97539b47b
heeyahnuh123 <heeyahnuh@gmail.com>
2022-03-12 16:58:16 -0800

    commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-    char ch;
+	char ch;
 
-    for (ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
     {
-        putchar(ch);
+	putchar(ch);
     }
-    putchar('\n');
-    return (0);
+	putchar('\n');
+	return (0);
 }
