d0dba1657eedcbd2285e3fa613800c84b1dd960f
heeyahnuh123 <heeyahnuh@gmail.com>
2022-03-15 08:44:08 -0700

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
-#include "_putchar.h"
+#include "alx.h"
 
 /**
- * print_alphabet_x10 - Check holberton
+ * print_alphabet_x10
  * Description: function uses _putchar function to print
  * alphabet in lowercase 10 times
  * Return: Nothing.
